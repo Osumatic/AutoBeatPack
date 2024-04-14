03b7a294259e28e7561fae7f6e1f45bbf49be46d
Saltssaumure <themes@saltssaumure.net>
2024-04-14 07:03:12 +0100

    Add rough dictionary of pack types

---
+++ lib/types.py
@@ -0,0 +1,58 @@
+"""
+Types of beatmap packs and their naming conventions
+"""
+
+pack_types = {
+    "standard": {
+        "example": "{prefix}{suffix}{num} - {mode} Beatmap Pack #{num}",
+        "prefix": "S",
+        "subtypes": {
+            "osu!": {
+                "suffix": "",
+                "mode": "osu!"
+            },
+            "osu!taiko": {
+                "suffix": "T",
+                "mode": "osu!taiko"
+            },
+            "osu!mania": {
+                "suffix": "M",
+                "mode": "osu!mania"
+            },
+            "osu!catch": {
+                "suffix": "C",
+                "mode": "osu!catch"
+            }
+        }
+    },
+    "featured artist": {
+        "example": "{prefix}{num} - {artistname} Pack",
+        "prefix": "F"
+    },
+    "tournament": {
+        "example": "{prefix}{num} - {tournamentname}: {stage} Pack",
+        "prefix": "P"
+    },
+    "loved": {
+        "example": "{prefix}{num} - Project Loved: {season} ({mode})",
+        "prefix": "L"
+    },
+    "spotlights": {
+        "example": "{prefix}{num} - Beatmap Spotlights {season} ({mode})",
+        "prefix": "R"
+    },
+    "theme": {
+        "example": "{prefix}{suffix}{num} - {packname}",
+        "prefix": "T",
+        "subtypes": {
+            "centurion": {
+                "example": "{prefix}{suffix}{num} - Centurion Pack: {mapper}",
+                "suffix": "M"
+            }
+        }
+    },
+    "artist/album": {
+        "example": "{prefix}{num} - {packname}",
+        "prefix": "A"
+    }
+}
